ddea5f97cb049fe246f1a35a81c7713ce073b2fd
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-21 17:39:53 +0700

    Add error and exception handling middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCoode = res.statusCoode ? res.statusCoode : 500;
+
+  res.status(statusCoode);
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+module.exports = { errorHandler };
