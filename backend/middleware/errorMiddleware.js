8a65d5459b18fe97a451afee75cccfd688cc3974
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-12-09 16:36:56 +0700

    Register user and hash password
    -Write register user functionality
    -Encrypt password with bcryptjs (salt then hash)

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,12 +1,12 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCoode = res.statusCoode ? res.statusCoode : 500;
+  const statusCode = res.statusCode ? res.statusCode : 500
 
-  res.status(statusCoode);
+  res.status(statusCode)
 
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
-};
+  })
+}
 
-module.exports = { errorHandler };
+module.exports = { errorHandler }
