1103022bbeae08992e32aa2f82db441330bb9265
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-12-13 14:18:29 +0700

    Protect goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const {
   getGoals,
   setGoal,
   updateGoal,
   deleteGoal,
-} = require('../controller/goalController');
+} = require('../controller/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
-module.exports = router;
+module.exports = router
